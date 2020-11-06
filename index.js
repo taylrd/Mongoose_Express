c38ca5aa9faefb36fa484ebdad5cbfd2e0a66cde
Corey Taylor <corey@taylrd.com>
2020-11-06 15:26:22 -0500

    init repo

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+
+mongoose
+  .connect('mongodb://localhost:27017/farmStand', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MONGO CONNECTION OPEN!');
+  })
+  .catch((err) => {
+    console.log('Oh NO MONGO Connecction ERROR!!!');
+    console.log(err);
+  });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/products', async (req, res) => {
+  const products = await Product.find({});
+  console.log(products);
+  res.render('products/index');
+});
+
+app.listen(3000, () => {
+  console.log('APP IS LISTENING ON PORT 3000!');
+});
